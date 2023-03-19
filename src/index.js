76ac120e31c323c1f54309338377df2d79c1efae
Diego-coder2525 <forgam2525@outlook.es>
2023-03-19 12:19:21 -0500

    First commit with greetings

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Greeting() {
+  return (
+    <React.Fragment>
+      <div>
+        <h3>asdasd</h3>
+        <ul>
+          <li>
+            <a href="#">asdads</a>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
+  );
+}
+//<div><h1>wasaa</h1></div>
+//React.createElement("h2", {}, "hello world")
+//React.createElement("div",{},React.createElement("h2", {}, "hello world")
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Greeting />);
